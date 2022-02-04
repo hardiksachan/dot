016e151d56078c07a51f8f9517a4f6ced22a5cac
Hardik Sachan <sachanhardik@gmail.com>
2022-02-04 20:16:45 +0530

    Basic polybar setup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -466,4 +466,5 @@ beautiful.useless_gap = 5
 -- Autostart apps
 awful.spawn.with_shell("picom")
 awful.spawn.with_shell("feh --bg-fill ~/Pictures/wallpapers/mountains1.jpg")
+awful.spawn.with_shell("~/.config/polybar/launchers/awesome.sh")
 
