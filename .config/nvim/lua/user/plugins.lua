fc6709ca4cd89078a481e6cde8aa0f2abae1e9be
Hardik Sachan <sachanhardik@gmail.com>
2022-02-08 14:20:20 +0530

    nvim: add toggleterm

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -65,6 +65,7 @@ return packer.startup(function(use)
 	use("akinsho/bufferline.nvim")
 	use("moll/vim-bbye")
 	use("nvim-lualine/lualine.nvim")
+	use("akinsho/toggleterm.nvim")
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
