3f9291e800ea4aaec0ab41c747956172e0820328
Hardik Sachan <sachanhardik@gmail.com>
2022-02-08 14:02:47 +0530

    nvim: add null-ls and formatters/linters for python, lua and ts

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -66,6 +66,9 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
+-- Null-LS format
+keymap("n", "<leader>l", ":Format<cr>", opts)
+
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
